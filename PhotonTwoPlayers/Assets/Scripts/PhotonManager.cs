4b93a7a77f574b1b1a4daccde15f53923293576c
pollenjp <polleninjp@gmail.com>
2018-10-28 20:28:09 +0900

    Update GetRoomLists

--- PhotonTwoPlayers/Assets/Scripts/PhotonManager.cs
+++ PhotonTwoPlayers/Assets/Scripts/PhotonManager.cs
@@ -9,6 +9,7 @@ public class PhotonManager : Photon.MonoBehaviour
 	private string _userName, _userId;
 	public InputField InputFieldUserId;
 	public GameObject Menu1, Menu2;
+	public Text RoomList;
 
 	// Use this for initialization
 	private void Start () {
@@ -31,6 +32,7 @@ public class PhotonManager : Photon.MonoBehaviour
 	{
 		Debug.Log ("PhotonManager OnJoinedLobby");
 		ChangeCanvas(canvasGameObject1: Menu1, canvasGameObject2: Menu2);
+		//ShowRoomLists();
 	}
 	//########################################
 	// Connect server Menu --> Join Room Menu
@@ -45,7 +47,21 @@ public class PhotonManager : Photon.MonoBehaviour
 	{
 		Debug.Log(message: "===PhotonNetwork.GetRoomList===\n" +
 		                   "ルーム一覧を表示\n" + PhotonNetwork.GetRoomList() + "\n\n");
+		RoomList.text = "";  // init
+		foreach (var roomInfo in PhotonNetwork.GetRoomList())
+		{
+			Debug.Log(roomInfo.Name);
+			RoomList.text += roomInfo.ToString();
+		}
+
+		Debug.Log(RoomList.text);
 	}
+	
+	public void OnReceivedRoomListUpdate() 
+	{
+		ShowRoomLists();
+	} 
+
 	
 	//########################################
 	// Check UserId(Name)
@@ -56,15 +72,29 @@ public class PhotonManager : Photon.MonoBehaviour
 		Debug.Log(_userId);
 	}
 
+	public void CreateRoomAndJoin()
+	{
+		var roomName = "test01";
+		CreateRoom(roomName: roomName);
+		//PhotonNetwork.JoinRoom(roomName: roomName);
+	}
+	
+	public void OnPhotonCreateRoomFailed()
+	{
+		Debug.Log("OnPhotonCreateRoomFailed got called. This can happen if the room exists (even if not visible). Try another room name.");
+		PhotonNetwork.JoinRoom(roomName: "test01");
+	}
+
+
 	// ルームの作成
-	public void CreateRoomButton(){
-		GetUserInfo();
+	public void CreateRoom(string roomName){
+		//GetUserInfo();
 		
 		// ref: http://sleepnel.hatenablog.com/entry/2016/05/29/120200
 		var userName = "userName1";
 		var userId   = "userId1";
-		var RoomName = "test01";
-		PhotonNetwork.autoCleanUpPlayerObjects = false;
+		
+		//PhotonNetwork.autoCleanUpPlayerObjects = false;
 		//カスタムプロパティ
 		ExitGames.Client.Photon.Hashtable customProp = new ExitGames.Client.Photon.Hashtable();
 		customProp.Add ("userName", userName); //ユーザ名
@@ -74,14 +104,13 @@ public class PhotonManager : Photon.MonoBehaviour
 		RoomOptions roomOptions = new RoomOptions ();
 		roomOptions.CustomRoomProperties = customProp;
 		//ロビーで見えるルーム情報としてカスタムプロパティのuserName,userIdを使いますよという宣言
-		roomOptions.CustomRoomPropertiesForLobby = new string[]{ "UserName","userId"};
+		roomOptions.CustomRoomPropertiesForLobby = new string[]{ "userName","userId"};
 		roomOptions.MaxPlayers = 2; //部屋の最大人数
 		roomOptions.IsOpen = true; //入室許可する
 		roomOptions.IsVisible = true; //ロビーから見えるようにする
 		//userIdが名前のルームがなければ作って入室、あれば普通に入室する。
-		Debug.Log(message: "====JoinOrCreateRoom==");
-		PhotonNetwork.JoinOrCreateRoom(roomName: userId, roomOptions: roomOptions, typedLobby: null);
+		Debug.Log(message: "===JoinOrCreateRoom===");
+		PhotonNetwork.JoinOrCreateRoom(roomName: roomName, roomOptions: roomOptions, typedLobby: null);
+		Debug.Log(message: "===JoinOrCreateRoom===");
 	}
-
-
 }
