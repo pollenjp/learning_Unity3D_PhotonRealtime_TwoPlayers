34ecf96d5017c87414ec04dba63a34bf20f473b7
pollenjp <polleninjp@gmail.com>
2018-10-29 17:34:09 +0900

    Update temporary commit

---
+++ PhotonTwoPlayers/Assets/Scripts/LobbyUIScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUIScript : MonoBehaviour {
+	
+	//部屋作成ウインドウ表示用ボタン
+	public Button OpenRoomPanelButton;
+ 
+	//部屋作成ウインドウ
+	public GameObject CreateRoomPanel;    //部屋作成ウインドウ
+	public Text       RoomNameText;       //作成する部屋名
+	public Slider     PlayerNumberSlider; //最大入室可能人数用Slider
+	public Text       PlayerNumberText;   //最大入室可能人数表示用Text
+	public Button     CreateRoomButton;   //部屋作成ボタン
+	
+	//########################################
+	//部屋作成ウインドウ表示用ボタンを押したときの処理
+	public void OnClick_OpenRoomPanelButton()
+	{
+		// 部屋作成ウインドウが表示していれば => 部屋作成ウインドウを非表示に
+		// そうでなければ                 => 部屋作成ウインドウを表示
+		if (CreateRoomPanel.activeSelf)
+		{
+			CreateRoomPanel.SetActive(false);
+		}
+		else
+		{
+			CreateRoomPanel.SetActive(true);
+		}
+	}
+	//########################################
+	//部屋作成ボタンを押したときの処理
+	public void OnClick_CreateRoomButton()
+	{
+		//作成する部屋の設定
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.IsVisible = true;   //ロビーで見える部屋にする
+		roomOptions.IsOpen = true;      //他のプレイヤーの入室を許可する
+		roomOptions.MaxPlayers = (byte)PlayerNumberSlider.value;    //入室可能人数を設定
+		//ルームカスタムプロパティで部屋作成者を表示させるため、作成者の名前を格納
+		roomOptions.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable()
+		{
+			{ "RoomCreator",PhotonNetwork.playerName }
+		};
+		//ロビーにカスタムプロパティの情報を表示させる
+		roomOptions.CustomRoomPropertiesForLobby = new string[] {
+			"RoomCreator",
+		};
+ 
+		//部屋作成
+		PhotonNetwork.CreateRoom(RoomNameText.text,roomOptions,null);
+	}
+
+	//####################################################################################################
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		//部屋人数Sliderの値をTextに代入
+		PlayerNumberText.text = PlayerNumberSlider.value.ToString();
+	}
+ 
+}
---
+++ PhotonTwoPlayers/Assets/Scripts/RoomElementScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomElementScript : MonoBehaviour {
+
+	//Room情報UI表示用
+	public Text RoomName;      //部屋名
+	public Text PlayerNumber;  //人数
+	public Text RoomCreator;   //部屋作成者名
+ 
+	//入室ボタン_roomName格納用
+	private string _roomName;
+ 
+	//####################################################################################################
+	// GetRoomListからRoom情報をRoomElementにセットしていくための関数
+	public void SetRoomInfo(string roomName,int playerNumber,int maxPlayer,string roomCreator)
+	{
+		//入室ボタン用_roomName取得
+		_roomName         = roomName;
+		RoomName.text     = "部屋名：" + roomName;
+		PlayerNumber.text = "人　数：" + playerNumber + "/" + maxPlayer;
+		RoomCreator.text  = "作成者：" + roomCreator;
+	}
+	//########################################
+	//入室ボタン処理
+	public void OnJoinRoomButton()
+	{
+		//_roomNameの部屋に入室
+		PhotonNetwork.JoinRoom(_roomName);
+	}
+	
+	//####################################################################################################
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+}
